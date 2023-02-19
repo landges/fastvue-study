136a3b4579420f5b2a985c56c5e96eedca4c499c
Alex Berizut <59904018+landges@users.noreply.github.com>
2023-02-19 06:10:07 -0800

    backend with db

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
+from backend import models, routers
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from .database import engine
+
+models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
@@ -12,12 +17,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/posts")
-async def root():
-    posts={ "posts":[
-            {"id": 1, "title": "post1", "discriprion":"first post in vue"},
-            {"id": 2, "title": "post2", "discriprion":"second post in vue"},
-            {"id": 3, "title": "post3", "discriprion":"thirth post in vue"},
-        ]          
-    }
-    return JSONResponse(content=posts)
+app.include_router(routers.router, tags=['Notes'], prefix='/api')
---
+++ backend/routers.py
@@ -0,0 +1,16 @@
+from . import models
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status, APIRouter, Response
+from .database import get_db
+from fastapi.responses import JSONResponse
+
+router = APIRouter()
+
+
+@router.get("/posts")
+async def root(db: Session = Depends(get_db), limit: int = 10, page: int = 1,search: str = ''):
+    skip = (page - 1) * limit
+
+    posts = db.query(models.Note).filter(
+        models.Note.title.contains(search)).limit(limit).offset(skip).all()
+    return {'status': 'success', 'results': len(posts), 'notes': posts}
